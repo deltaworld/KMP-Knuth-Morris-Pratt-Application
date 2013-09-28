259d4a1782710252166d66cc77d0416f9aefbc0d
Tareq Fadel <itrainit@gmail.com>
2013-09-28 01:11:16 +0100

    first commit

---
+++ RenderApplication/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace RenderApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.result_table = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // result_table
+            // 
+            this.result_table.ColumnCount = 3;
+            this.result_table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.59399F));
+            this.result_table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.59399F));
+            this.result_table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.81203F));
+            this.result_table.Location = new System.Drawing.Point(12, 44);
+            this.result_table.Name = "result_table";
+            this.result_table.RowCount = 3;
+            this.result_table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.result_table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.result_table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.result_table.Size = new System.Drawing.Size(260, 205);
+            this.result_table.TabIndex = 0;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.result_table);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel result_table;
+    }
+}
+
---
+++ RenderApplication/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenderApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        List<int> pi = new List<int> { 0, 0, 0, 1, 2, 3 };
+        String input = "abxyab";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            Label label1 = new Label();
+            Label label2 = new Label();
+            Label label3 = new Label();
+            label1.Text = "i";
+            label2.Text = "S[i]";
+            label3.Text = "pi[i]";
+
+            result_table.RowCount = 1;
+            result_table.Controls.Add(label1, 0, result_table.RowCount);
+            result_table.Controls.Add(label2, 1, result_table.RowCount);
+            result_table.Controls.Add(label3, 2, result_table.RowCount);
+            render(pi, input);
+            
+        }
+        private void render(List<int> pi, string input) // tareq output function.
+        {
+
+            /* Debug */
+            //pi.ForEach(i => Console.Write("{0}\t", i));       // output all list items to console.
+
+            List<char> subStringList = input.ToList();          // converts input string to List
+
+            /* Debug */
+            // subStringList.ForEach(i => Console.Write("{0}\t", i));  // output all list items to console.
+            // Console.Write(subStringList.Count); // size of list
+            // Console.Write(subStringList.Count);
+            // Console.Write(result_table.RowCount);
+
+            /* Pauses the render of the table until all data is dynamically populated in memory
+               If suspend and resume is removed causes a delay. */
+            result_table.SuspendLayout();
+            result_table.RowCount++;
+
+            /* Loop through length of input pattern */
+            for (int i = 0; i < subStringList.Count; i++)
+            {
+                Label iLabel = new Label();         // Label for iteration counter  e.g. 3
+                Label sLabel = new Label();         // Label for substring display  e.g. abc   
+                Label piLabel = new Label();        // Label for prefix value       e.g. 1
+
+                iLabel.Text = Convert.ToString(i + 1);               // Convert to String and add 1 (counter from 0)
+                sLabel.Text += Convert.ToString(subStringList[i]);  // Concatenate result to increase substring for each row.
+                piLabel.Text = Convert.ToString(pi[i]);             // prefix of index position of substring.
+
+                /* Adds Labels to the TableLayoutPanel */
+                result_table.Controls.Add(iLabel, 0 /*col*/, result_table.RowCount /*row*/);
+                result_table.Controls.Add(sLabel, 1 /*col*/, result_table.RowCount /*row*/);
+                result_table.Controls.Add(piLabel, 2 /*col*/, result_table.RowCount /*row*/);
+                result_table.RowCount++;                            // Next Row, Next char in substring
+
+                /* Debug */
+                Console.Write(result_table.RowCount);
+            }
+            result_table.ResumeLayout(); // Renders the layout to screen.
+        }
+
+    }
+}
