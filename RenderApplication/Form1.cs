a3176605ff4fb7801ea16e68a031d7de488acc59
deltaworld <itrainit@gmail.com>
2013-10-02 12:24:22 +0100

    Update Form1.cs
    Add Clear button (Chris)
    Mod PrefixArray to convert to List
    Add PrefixList

--- RenderApplication/Form1.cs
+++ RenderApplication/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,18 +16,15 @@ namespace RenderApplication
         {
             InitializeComponent();
         }
-
-        
-
-        List<int> pi = new List<int> { 0, 0, 0, 1, 2, 3 };
-        String input = "abxyab";
+        //List<int> pi = new List<int> { 0, 0, 0, 1, 2, 3 };
+        String input = "abcdabd";
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
             Label label1 = new Label();
             Label label2 = new Label();
             Label label3 = new Label();
+
             label1.Text = "i";
             label2.Text = "S[i]";
             label3.Text = "pi[i]";
@@ -36,12 +33,70 @@ namespace RenderApplication
             result_table.Controls.Add(label1, 0, result_table.RowCount);
             result_table.Controls.Add(label2, 1, result_table.RowCount);
             result_table.Controls.Add(label3, 2, result_table.RowCount);
-            render(pi, input);
             
+            PrefixArray(input);
+            render(prefixList(input), input);   
         }
+
+        private List<int> prefixList(string w)
+        { 
+            List<int> p = new List<int>();
+            p.Add(0);
+            int j = 0;
+            for (int i = 1; i < w.Length; i++) {
+                while (j > 0 && w[j] != w[i])
+                    j = p[j-1];
+                
+                if (w[j] == w[i])
+                    j++;
+                p.Add(j);
+            }
+            p.ForEach(n => Console.Write("{0}\t", n));
+            return p;
+        }
+
+        private List<int> PrefixArray(string w)
+        {
+            //List<char> subStringList = w.ToList();
+
+            int[] t = new int[w.Length];
+            int i = 2;
+            int j = 0;
+            t[0] = -1;
+            t[1] = 0;
+
+            while (i < w.Length)
+            {
+                if (w[i - 1] == w[j])
+                {
+                    t[i] = j + 1;
+                    j++;
+                    i++;
+                }
+                else if (j > 0)
+                {
+                    j = t[j];
+                }
+                else
+                {
+                    t[i] = 0;
+                    i++;
+                    j = 0;
+                }
+            }
+            //Console.Write("Hello");
+            //Console.Write(string.Join(",", t));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            List<int> lst = t.OfType<int>().ToList();
+            //lst.ForEach(n => Console.Write("{0}\t", n));
+            
+            return lst;
+        }
+
         private void render(List<int> pi, string input) // tareq output function.
         {
-
             /* Debug */
             //pi.ForEach(i => Console.Write("{0}\t", i));       // output all list items to console.
 
@@ -81,5 +136,27 @@ namespace RenderApplication
             result_table.ResumeLayout(); // Renders the layout to screen.
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();               //Clears textbox
+            result_table.Controls.Clear();  //Clears the result table 
+            result_table.RowCount--;        //Decreases the number of rows in table 
+
+            Label label1 = new Label();     
+            Label label2 = new Label();
+            Label label3 = new Label();
+
+            label1.Text = "i";
+            label2.Text = "S[i]";
+            label3.Text = "pi[i]";
+
+            label1.Font = new Font(label1.Font.Name, 8, FontStyle.Bold);
+            label2.Font = new Font(label2.Font.Name, 8, FontStyle.Bold);
+            label3.Font = new Font(label3.Font.Name, 8, FontStyle.Bold);
+
+            result_table.Controls.Add(label1, 0, result_table.RowCount);
+            result_table.Controls.Add(label2, 1, result_table.RowCount);
+            result_table.Controls.Add(label3, 2, result_table.RowCount);
+        }
     }
 }
